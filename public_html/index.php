e335d87c900f9c5fce3a6a29f53552eb4320122e
Jonathan Guzman <jguzman41@cnm.edu>
2016-10-12 12:56:55 -0600

    Added CSS, img, and Index html

---
+++ public_html/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+
+		<!-- add CSS file -->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css"  />
+		<title> Data Design Assignment</title>
+	</head>
+	<body>
+		<!-- this is the header-->
+		<header>
+			<h1>Soundcloud. Yep, anything can be music now.</h1>
+		</header>
+
+		<!-- this is the main page content-->
+		<main>
+
+			<!-- begin section 1-->
+
+			<section>
+				<h1>Persona</h1>
+				<img src="img/yungdust.jpg" alt="YungDust">
+				<p>This is Daniel. He owns a fairly recent Mac. He makes music. He's a Computer Science student at UNM who is interested in sound design and music production.He wants to upload his music to soundcloud so that his friends and potential fans can listen to it. He wants to make his music easily to locate amongst billions of hours of sound uploaded to Soundcloud. Luckily, a feature exists that allows him to do just that... Tagging! Tagging will help listeners who have found other similar artists find him. It will also help those who listen to his music link out into other similar songs.</p>
+
+				<h3>Some of his favorite tags include</h3>
+				<ul>
+					<li>#HipHop</li>
+					<li>#Rap</li>
+					<li>#HipHop-Rap</li>
+				</ul>
+			</section>
+
+			<!-- begin section 2-->
+
+			<section>
+				<h1>Use Case</h1>
+				<p> After promising a new mixtape release at midnight to all of his die hard fan and twitter followers, Daniel logs in at 11:57 and prepares the upload. While the files upload, he fills out the rest of the fields in the upload process. this includes his tags. Once everything is done uploading, he is able to attach tags tothe abum as a whole, as well as attach tags to each individual song. Finally, he selects "submit" and the new music is realeased upon the world!</p>
+			</section>
+
+
+			<!-- begin section 3-->
+			<section>
+				<h1>Entities</h1>
+				<h3>The entities involved include:</h3>
+					<ul>
+						<li>Tags</li>
+						<li>Songs</li>
+						<li>Profile</li>
+					</ul>
+
+				<iframe width="50%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/38377533&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+			</section>
+
+		</main>
+	</body>
+</html>
