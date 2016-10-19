696697aa9030d7ac80e6ce0b8bf2e82cf9bebfeb
Jonathan Guzman <jguzman41@cnm.edu>
2016-10-19 11:21:23 -0600

    added php directory

--- sql/data-design-soundcloud.sql
+++ sql/data-design-soundcloud.sql
@@ -1,25 +1,43 @@
-DROP TABLE IF EXISTS 'tag';
-DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS tag;
 DROP TABLE IF EXISTS song;
+DROP TABLE IF EXISTS songTag;
+DROP TABLE IF EXISTS profile;
 
 CREATE TABLE profile (
 	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	profileFollowers INT UNSIGNED NOT NULL,
-	UNIQUE (profileId),
+	profileName VARCHAR (32) NOT NULL,
+	profileEmail VARCHAR(128) NOT NULL,
+	UNIQUE (profileName),
+	UNIQUE (profileEmail),
+	INDEX (profileId),
 	PRIMARY KEY (profileId)
 );
 
 CREATE TABLE song (
-	songProfileId VARCHAR(32) NOT NULL,
-	songDateTime DATETIME NOT NULL,
 	songId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	songProfileId INT UNSIGNED NOT NULL,
+	songDateTime DATETIME NOT NULL,
+	INDEX (songId),
+# 	INDEX (profileId),
+	FOREIGN KEY (songProfileId) REFERENCES profile(profileId),
 	PRIMARY KEY (songId)
 );
 
-CREATE TABLE 'tag' (
-	tagLabel VARCHAR(16),
+CREATE TABLE tag (
+	tagProfileId INT UNSIGNED NOT NULL,
+	tagSongId INT UNSIGNED NOT NULL,
+	tagLabel VARCHAR(32),
 	INDEX (tagLabel),
-	FOREIGN KEY (tagLabel), REFERENCES song(songProfileId),
-	PRIMARY KEY (tagLabel)
-
+	UNIQUE (tagLabel),
+	FOREIGN KEY (tagProfileId) REFERENCES profile(profileId),
+	PRIMARY KEY (tagProfileId)
 );
+
+CREATE TABLE songTag (
+	songTagProfileId INT UNSIGNED NOT NULL,
+	songTagSongId INT UNSIGNED NOT NULL,
+	FOREIGN KEY (songTagProfileId) REFERENCES profile(profileId),
+	FOREIGN KEY (songTagSongId) REFERENCES song(songId),
+	PRIMARY KEY (songTagProfileId, songTagSongId)
+);
+
